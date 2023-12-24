379199f99a04a9effab5d7c9ebc77d09937979bc
Taisuke Fukuno <fukuno@jig.jp>
2023-12-24 19:13:33 +0900

    remove for nodejs

--- nodejs/base64.js
+++
@@ -1,8 +0,0 @@
-import { Base64URL } from "https://code4fukui.github.io/Base64URL/Base64URL.js";
-import { Buffer } from "https://taisukef.github.io/buffer/Buffer.js";
-
-export const base64 = {
-  encode: (b) => Base64URL.encode(b),
-  decode: (s) => Buffer.from(Base64URL.decode(s)),
-};
-
